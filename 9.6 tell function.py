2ec489de5b85fb7113601ed53d313ff5e612a07a
akirah2000 <mounikopparapu28@gmail.com>
2025-06-04 12:13:56 +0530

    Add files via upload

---
+++ 9.6 tell function.py
@@ -0,0 +1,5 @@
+f=open("c:\\users\\Administrator s\\Desktop\\pythonlab\\8.5 file and write,read content.py","r")
+print("the file pointer is at byte:",f.tell())
+content=f.read();
+print("After reading,the file pointr is at:",f.tell())
+
